6219a384fd072b7717ebe4ae6c91f64bd4808eab
dunegxn <arpitxdungeon@gmail.com>
2024-11-30 23:04:03 +0530

    initial-passkey-work

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import {
   View,
   Text,
@@ -6,17 +6,37 @@ import {
   ImageBackground,
   TouchableOpacity,
   Image,
+  Button,
+  TextInput
 } from "react-native";
 import { router } from "expo-router";
+import * as SecureStore from 'expo-secure-store';
+
 
 import { Theme } from "@/constants/Colors";
 
 import { Card, CardFooter } from "../ui/Card";
 
 const Hero = () => {
+  const [key, onChangeKey] = useState('Your key here');
+  const [value, onChangeValue] = useState('Your value here');
+
   const handleShopCTAClick = useCallback(() => {
     router.navigate("/(shop)");
   }, []);
+
+  const save = async (key, value) => {
+    await SecureStore.setItemAsync(key, value);
+  }
+
+  const getValueFor = async (key) => {
+    let result = await SecureStore.getItemAsync(key);
+    if (result) {
+      alert("🔐 Here's your value 🔐" + result);
+    } else {
+      alert('No values stored under that key.');
+    }
+  }
 
   return (
     <Card style={styles.card}>
@@ -44,6 +64,41 @@ const Hero = () => {
           <Text style={styles.heroButtonText}>Shop</Text>
         </TouchableOpacity>
       </CardFooter>
+      <View>
+
+
+      <TextInput
+              style={styles.textInput}
+              clearTextOnFocus
+              onChangeText={text => onChangeKey(text)}
+              value={key}
+            />
+            <TextInput
+              style={styles.textInput}
+              clearTextOnFocus
+              onChangeText={text => onChangeValue(text)}
+              value={value}
+            />
+
+      <Button
+              title="Save this key/value pair"
+              onPress={() => {
+                save(key, value);
+                onChangeKey('Your key here');
+                onChangeValue('Your value here');
+              }}
+            />
+
+
+            <Text style={styles.paragraph}>🔐 Enter your key 🔐</Text>
+            <TextInput
+              style={styles.textInput}
+              onSubmitEditing={event => {
+                getValueFor(event.nativeEvent.text);
+              }}
+              placeholder="Enter the key for the value you want to get"
+            />
+      </View>
     </Card>
   );
 };
